26e9bd4b55198a50dcbda6cd9769ae4ac9a34938
Saurabh-Sarode-IOE <saurabhs.aids_ioe@bkc.met.edu>
2025-04-20 23:12:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 from model import predict_disease  # Ensure this is correctly implemented
 from flask_cors import CORS
 
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 app = Flask(__name__)
 CORS(app)
 
